be905de09e3622b75deb8b6ac037dd18d6f19dec
Rafael Cronemberger <rafael@berger.com.br>
2025-01-15 03:34:26 -0300

    Package updates

--- src/Berger.Extensions.Graph/Models/Node.cs
+++ src/Berger.Extensions.Graph/Models/Node.cs
@@ -4,7 +4,11 @@ namespace Berger.Extensions.Graph
 {
     public class Node : BaseEntity
     {
-        public string Type { get; set; }
+        public Guid NodeTypeId { get; set; }
+        public NodeType NodeType { get; set; }
+        public Guid? EntityTypeId { get; set; }
+        public Guid? EntityId { get; set; }
+        public string Name { get; set; }
         public string Label { get; set; }
         public string Description { get; set; }
         public string Icon { get; set; }
---
+++ src/Berger.Extensions.Graph/Models/NodeType.cs
@@ -0,0 +1,10 @@
+﻿using Berger.Extensions.Abstractions;
+
+namespace Berger.Extensions.Graph
+{
+    public class NodeType : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
